592494e821c5f66174bf75d186bb230fce906bdc
Sripathi Krishnan <Sripathi.Krishnan@gmail.com>
2018-01-16 17:07:55 +0530

    More robust implementation of download_rdb

--- remote_dump.py
+++ remote_dump.py
@@ -1,39 +1,38 @@
-import redis
+from redis.connection import Connection
 
-def dict_merge(*dicts):
-    merged = {}
-    for d in dicts:
-        merged.update(d)
-    return merged
-
-
-def parse_sync_response(*args):
-    with open("remote_dump.rdb", "wb") as f:
-        f.write(args[0])
-    return None
-
-class RedisWithSync(redis.StrictRedis):
-    # Overridden callbacks
-    RESPONSE_CALLBACKS = dict_merge(
-        redis.StrictRedis.RESPONSE_CALLBACKS,
-        {
-            'SYNC': parse_sync_response,
-        }
-    )
-
-    def sync(self):
-        return self.execute_command('SYNC')
-
-
-def download_dump(local_path, host, port, username=None, password=None):
+def download_rdb(out_file, host='localhost', port=6379, password=None):
     """
     Download a rdb dump file from a remote redis server
-    local_path is the path where the rdb will be saved
-    host, port, username and password are used 
-    to connect to the remote redis server
+    out_file is the file name where the rdb will be saved
+    host, port, password are used to connect to the remote redis server
     """
-    remote_redis = RedisWithSync(host, port, username, password)
-    print(remote_redis.sync())
+    try:
+        conn = _MyConnection(host=host, port=port, password=password)
+        conn.send_command("sync")
+
+        with open(out_file, "wb") as out:
+            conn.read_file(out)
+    finally:
+        conn.disconnect()
+
+class _MyConnection(Connection):
+    def read_file(self, out):
+        rdb_length = self.read_rdb_length()
+
+        buff_length = 16384
+        remaining = rdb_length
+        while(remaining > 0):
+            partial = self._sock.recv(min(buff_length, remaining))
+            remaining = remaining - len(partial)
+            out.write(partial)
+
+    def read_rdb_length(self):
+        # Read till we encounter \n in the socket
+        data = b''.join(iter(lambda: self._sock.recv(1), b'\n'))
+        # The first character is the $ symbol, skip it
+        # Everything after that is the length of the file
+        length = int(data[1:])
+        return length
 
 if __name__ == '__main__':
-    download_dump("dump.rdb", "localhost", 6379)
+    download_rdb("dump.rdb")
